c857b5c750d8cdaf3b88454786116e24152f89a9
Leonardo Azeredo <leonardo.azeredo@live.com>
2018-09-03 20:51:18 -0300

    started tasks crud

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @tasks = ["Be cool","say no to drugs"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  resources :tasks
 end
